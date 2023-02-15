4d7fba631a881242d288c18517a540fd64074a24
Ethan Pinto <ethanpintoa@gmail.com>
2023-02-15 17:28:28 -0500

    Resize and desaturate image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use image::imageops::FilterType;
+
 fn main() {
-	let _img = image::open("image.png").unwrap();
+	let img = image::open("image.png").unwrap();
+
+	let img = img.resize_exact(100, 50, FilterType::Nearest);
+	let _image_buf = img.to_luma8();
 }
