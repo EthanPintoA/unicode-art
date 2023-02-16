69c85271a7e93e5716394fb97a7a9193b6ab5ede
Ethan Pinto <ethanpintoa@gmail.com>
2023-02-16 03:11:38 -0500

    Add cli

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 mod ascii_art;
 
+use std::path::PathBuf;
+
 use ascii_art::image_to_ascii;
+use clap::{arg, value_parser, Command};
 
 fn main() {
-	let img = image::open("image.png").unwrap();
+	let matches = Command::new(env!("CARGO_PKG_NAME"))
+		.version(env!("CARGO_PKG_VERSION"))
+		.about("Generates ascii art")
+		.arg(arg!([FILE] "Image file").value_parser(value_parser!(PathBuf)))
+		.arg(arg!(-w --width <width> "Width of ascii art").value_parser(value_parser!(u32)))
+		.get_matches();
 
-	let width = 100;
+	let img_path = matches.get_one::<PathBuf>("FILE").unwrap();
+	let width = *matches.get_one::<u32>("width").unwrap_or(&100);
+
+	let img = image::open(img_path).unwrap();
+
 	let ratio = img.height() as f32 / img.width() as f32;
 	let height = ((width as f32 * ratio) / 2.0).round() as u32;
 
