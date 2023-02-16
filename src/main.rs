80fc9c6fa600988a07b606a3d09a1e6bbea47987
Ethan Pinto <ethanpintoa@gmail.com>
2023-02-16 02:15:21 -0500

    Resize by ratio

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ use ascii_art::image_to_ascii;
 fn main() {
 	let img = image::open("image.png").unwrap();
 
-	let ascii_art = image_to_ascii(img, 100, 50);
+	let width = 100;
+	let ratio = img.height() as f32 / img.width() as f32;
+	let height = ((width as f32 * ratio) / 2.0).round() as u32;
+
+	let ascii_art = image_to_ascii(img, width, height);
 
 	println!("{}", ascii_art)
 }
